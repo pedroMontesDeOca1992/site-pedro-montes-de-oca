05711739ce646cb68e430d30ba6e985782eecc61
pedromontesdeoca1992 <pedroMontesDeOca1992@users.noreply.github.com>
2021-04-09 23:46:03 -0500

    Add files via upload

--- js/maquina.js
+++ js/maquina.js
@@ -1,1 +1,19 @@
-var typed=new Typed('.typed',{strings:['<i class="textos">Mi nombre es pedro montes de oca</i>','<i class="textos">soy ingeniero en sistemas computacionales.</i>',],stringsElement:'#cadenas-texto',typeSpeed:75,startDelay:400,backSpeed:75,smartBackspace:true,shuffle:false,backDelay:1500,loop:true,loopCount:false,showCursor:true,cursorChar:'|',contentType:'html',});
+var typed = new Typed('.typed', {
+	strings: [
+		'<i class="textos">Mi nombre es pedro montes de oca</i>',
+		'<i class="textos">soy ingeniero en sistemas computacionales.</i>',
+
+	],
+	stringsElement: '#cadenas-texto', // ID del elemento que contiene cadenas de texto a mostrar.
+	typeSpeed: 75, // Velocidad en mlisegundos para poner una letra,
+	startDelay: 400, // Tiempo de retraso en iniciar la animacion. Aplica tambien cuando termina y vuelve a iniciar,
+	backSpeed: 75, // Velocidad en milisegundos para borrrar una letra,
+	smartBackspace: true, // Eliminar solamente las palabras que sean nuevas en una cadena de texto.
+	shuffle: false, // Alterar el orden en el que escribe las palabras.
+	backDelay: 1500, // Tiempo de espera despues de que termina de escribir una palabra.
+	loop: true, // Repetir el array de strings
+	loopCount: false, // Cantidad de veces a repetir el array.  false = infinite
+	showCursor: true, // Mostrar cursor palpitanto
+	cursorChar: '|', // Caracter para el cursor
+	contentType: 'html', // 'html' o 'null' para texto sin formato
+});
